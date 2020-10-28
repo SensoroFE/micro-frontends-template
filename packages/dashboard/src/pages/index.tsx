37add031f23a008854c0fc22420365c8885ed021
wangxingkang <156148958@qq.com>
2020-10-28 17:19:31 +0800

    feat(dashboard): 首页模块完毕

--- packages/dashboard/src/pages/index.tsx
+++ packages/dashboard/src/pages/index.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import styles from './index.less';
+import React, { Suspense } from 'react';
+import { Space, Row, Col } from 'antd';
+import { PageLoading } from '@ant-design/pro-layout';
 
-export default () => {
+const IntroduceRow = React.lazy(() => import('./components/introduce-row'));
+const SalesCard = React.lazy(() => import('./components/sales-card'));
+const TopSearch = React.lazy(() => import('./components/top-search'));
+const ProportionSales = React.lazy(() => import('./components/proportion-sales'));
+
+const Dashboard: React.FC = () => {
   return (
-    <div>
-      <h1 className={styles.title}>Dashboard page</h1>
-    </div>
-  );
+    <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+      <Suspense fallback={<PageLoading />}>
+        <IntroduceRow  />
+      </Suspense>
+
+      <Suspense fallback={null}>
+        <SalesCard />
+      </Suspense>
+
+      <Row gutter={16}>
+        <Col xl={12} lg={24} md={24} sm={24} xs={24}>
+          <Suspense fallback={null}>
+            <TopSearch />
+          </Suspense>
+        </Col>
+        <Col xl={12} lg={24} md={24} sm={24} xs={24}>
+          <Suspense fallback={null}>
+            <ProportionSales />
+          </Suspense>
+        </Col>
+      </Row>
+    </Space>
+  )
 }
+
+export default Dashboard;
